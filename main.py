14227d23d81ff42ce6b18a8ecc770494a0edfe8a
John000Smith <ImanolM.Quintela@gmail.com>
2024-11-24 20:46:24 +0100

    cp

--- main.py
+++ main.py
@@ -181,41 +181,41 @@ with col2:
             name=f'Punto ({x}, {y})'
         ))
         
-    # fig.add_trace(go.Scatter(
-    # x=[-1, -1], 
-    # y=[-8, 16],  # Desde el rango mínimo hasta el máximo en y
-    # mode='lines',
-    # line=dict(color='white', dash='dash'),
-    # name=f'Línea vertical (x={x})'
-    # ))
+    fig.add_trace(go.Scatter(
+    x=[-1, -1], 
+    y=[-8, 16],  # Desde el rango mínimo hasta el máximo en y
+    mode='lines',
+    line=dict(color='white', dash='dash'),
+    name=f'Línea vertical (x={x})'
+    ))
     
-    # fig.add_trace(go.Scatter(
-    # x=[1, 1], 
-    # y=[-8, 16],  # Desde el rango mínimo hasta el máximo en y
-    # mode='lines',
-    # line=dict(color='white', dash='dash'),
-    # name=f'Línea vertical (x={x})'
-    # ))
+    fig.add_trace(go.Scatter(
+    x=[1, 1], 
+    y=[-8, 16],  # Desde el rango mínimo hasta el máximo en y
+    mode='lines',
+    line=dict(color='white', dash='dash'),
+    name=f'Línea vertical (x={x})'
+    ))
     
-    # x_values = [x for x in range(-8, 9)]
-    # y_values = [x + 1 for x in x_values]  # Para una línea a 45º, y = x + 1
-    # fig.add_trace(go.Scatter(
-    #     x=x_values, 
-    #     y=y_values, 
-    #     mode='lines',
-    #     line=dict(color='blue', width=2, dash='dash'),
-    #     name="Línea a 45º desplazada"
-    # ))
+    x_values = [x for x in range(-8, 9)]
+    y_values = [x + 1 for x in x_values]  # Para una línea a 45º, y = x + 1
+    fig.add_trace(go.Scatter(
+        x=x_values, 
+        y=y_values, 
+        mode='lines',
+        line=dict(color='blue', width=2, dash='dash'),
+        name="Línea a 45º desplazada +1"
+    ))
     
-    # x_values = [x for x in range(-8, 9)]
-    # y_values = [x - 1 for x in x_values]
-    # fig.add_trace(go.Scatter(
-    #     x=x_values, 
-    #     y=y_values, 
-    #     mode='lines',
-    #     line=dict(color='blue', width=2, dash='dash'),
-    #     name="Línea a 45º desplazada"
-    # ))
+    x_values = [x for x in range(-8, 9)]
+    y_values = [x - 1 for x in x_values]
+    fig.add_trace(go.Scatter(
+        x=x_values, 
+        y=y_values, 
+        mode='lines',
+        line=dict(color='blue', width=2, dash='dash'),
+        name="Línea a 45º desplazada -1"
+    ))
     
     # x_values = [x for x in range(8, -9, -1)]
     # y_values = [x + 1 for x in x_values]
